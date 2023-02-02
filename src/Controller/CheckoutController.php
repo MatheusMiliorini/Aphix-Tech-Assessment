699805119284f0e6486e122d783a1b9c5bccf144
MatheusMiliorini <miliorinimatheus@gmail.com>
2023-02-02 13:21:09 +0000

    Add checkout page

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,16 +2,33 @@
 // src/Controller/CheckoutController.php
 namespace App\Controller;
 
+use App\Http\ProductsApiClient;
+use App\Services\PriceCalculator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CheckoutController extends AbstractController
 {
 
-    public function index(): Response
+    public function index(ProductsApiClient $productsApiClient, PriceCalculator $priceCalculator): Response
     {
-       return new Response(
-            '<html><body>Do your thing here!</body></html>'
-        );
+        $products = $productsApiClient->fetchCart();
+        $netTotal = $vatTotal = $total = 0;
+
+        foreach ($products as $product) {
+            $priceCalculator->calculate($product);
+            $netTotal += $product->netPrice;
+            $vatTotal += $product->vatPrice;
+            $total    += $product->totalPrice;
+        }
+
+        // https://twig.symfony.com/doc/2.x/filters/number_format.html
+
+        return $this->render('checkout.html.twig', [
+            'products' => $products,
+            'netTotal' => $netTotal,
+            'vatTotal' => $vatTotal,
+            'total'    => $total,
+        ]);
     }
 }
